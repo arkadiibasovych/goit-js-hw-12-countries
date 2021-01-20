0285815e368fa89ec4dc993de393d9af2e99ac13
arkadiibasovych <arkadiybasovich@gmai.com>
2021-01-20 05:19:46 +0200

    added worked files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import refs from './js/refs.js';
 import countryItems from './templates/countries-list.hbs';
 import countryCard from './templates/country-card.hbs';
 
+import "@pnotify/core/dist/PNotify.css"
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from "@pnotify/core";
+
 const _ = require('lodash');
 
 
@@ -23,7 +27,11 @@ refs.input.addEventListener('input', _.debounce((e) => {
             if (countries.length > 1 & countries.length < 11) { refs.countriesList.insertAdjacentHTML('beforeend', listMarkup) };
             refs.countryCard.innerHTML = "";
             if (countries.length === 1) { refs.countryCard.insertAdjacentHTML('beforeend', cardMarkup) };
-            if (countries.length > 10) { };
+            if (countries.length > 10) { error({
+            title: 'Too many matches found. Please enter a more spesific query!',
+            hide: true,
+            delay: 4000
+        })};
             
         })
     
